99927c883b3160ea4b64b70c2f141521a8731cf0
razaman2 <razamvmt@gmail.com>
2020-12-29 09:05:14 -0500

    removed unused docusign files

--- app/helpers/Docusign/Valley/templates/PVSResponsiblePersonsList.php
+++
@@ -1,7 +0,0 @@
-<?php
-    
-    namespace App\Helpers\Docusign\Valley\templates;
-
-    class PVSResponsiblePersonsList extends ResponsiblePersonsList {
-        protected $name = 'PVS Responsible Persons List';
-    }
--- app/helpers/Docusign/Valley/templates/PVSWaiverOfRightToCancel.php
+++
@@ -1,7 +0,0 @@
-<?php
-    
-    namespace App\Helpers\Docusign\Valley\templates;
-
-    class PVSWaiverOfRightToCancel extends WaiverOfRightToCancel {
-        protected $name = 'PVS Waiver of Right to Cancel';
-    }
