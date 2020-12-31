9b2836169dca3f4071e6f0c5c189c88741770f76
razaman2 <razamvmt@gmail.com>
2020-12-31 18:28:38 -0500

    updated npm packages

--- cloud-run/deploy/post_deploy.sh
+++ cloud-run/deploy/post_deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 cd /app || exit
-php artisan env
+
+env
 
 npm run prod
 
-php artisan telescope:install
-php artisan migrate
+#php artisan telescope:install
 
 sed -i "s,PORT,${PORT},g" /etc/nginx/nginx.conf
 sed -i "s,SERVER_NAME,${APP_NAME},g" /etc/nginx/nginx.conf
 
 # start the application
 php-fpm -D && nginx -g "daemon off;"
+
+php artisan migrate
