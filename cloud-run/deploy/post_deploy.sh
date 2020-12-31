84eb54507a9086dc03b8d171dabb1f4224e632bf
razaman2 <razamvmt@gmail.com>
2020-12-31 13:11:41 -0500

    deployment updates

--- cloud-run/deploy/post_deploy.sh
+++ cloud-run/deploy/post_deploy.sh
@@ -2,6 +2,13 @@
 
 echo "current working directory is currently."
 pwd
+cd /app || exit
+pwd
+php artisan env
+npm -v
+node -v
+env
+
 
 # start the application
 php-fpm -D && nginx -g "daemon off;"
