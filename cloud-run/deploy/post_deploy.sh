40dda5d6792dfd101bca8c71a0ac881c8715bcc3
razaman2 <razamvmt@gmail.com>
2020-12-31 13:59:55 -0500

    deployment updates

--- cloud-run/deploy/post_deploy.sh
+++ cloud-run/deploy/post_deploy.sh
@@ -9,7 +9,7 @@ php artisan telescope:install
 php artisan migrate
 
 sed -i "s,PORT,${PORT},g" /etc/nginx/nginx.conf
-sed -i "s,APP_URL,${APP_URL},g" /etc/nginx/nginx.conf
+sed -i "s,SERVER_NAME,${SERVER_NAME},g" /etc/nginx/nginx.conf
 
 # start the application
 php-fpm -D && nginx -g "daemon off;"
