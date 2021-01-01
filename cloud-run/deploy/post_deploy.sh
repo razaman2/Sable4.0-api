eb8bc2853bbc7a389a34f4fdb071bd2308fc10f3
razaman2 <razamvmt@gmail.com>
2021-01-01 01:11:20 -0500

    updated npm packages

--- cloud-run/deploy/post_deploy.sh
+++ cloud-run/deploy/post_deploy.sh
@@ -2,8 +2,8 @@
 
 cd /app || exit
 
-sed -i "s,PORT,${PORT},g" /etc/nginx/nginx.conf
-sed -i "s,SERVER_NAME,${APP_NAME},g" /etc/nginx/nginx.conf
+sed -i "s,SABLE_PORT,${PORT},g" /etc/nginx/nginx.conf
+sed -i "s,SABLE_SERVER_NAME,${APP_NAME},g" /etc/nginx/nginx.conf
 
 # start the application
 php-fpm -D && nginx -g "daemon off;"
