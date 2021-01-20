77621ca37f7119894dc6e0df8517395a9551869e
razaman2 <razamvmt@gmail.com>
2021-01-20 12:16:18 -0500

    updated response headers

---
+++ app/Http/Middleware/AddResponseHeaders.php
@@ -0,0 +1,25 @@
+<?php
+    namespace App\Http\Middleware;
+
+    use Closure;
+    use Illuminate\Http\Request;
+    use Symfony\Component\HttpFoundation\Response;
+
+    class AddResponseHeaders
+    {
+        public function handle($request, Closure $next) {
+            // Handle the request
+            $response = $next($request);
+
+            return $this->addHeaders($request, $response);
+        }
+
+        protected function addHeaders(Request $request, Response $response) : Response {
+            $response->headers->add([
+                'Content-Security-Policy' => 'upgrade-insecure-requests',
+                'Sable-Awesome-System' => 'testing-headers'
+            ]);
+
+            return $response;
+        }
+    }
