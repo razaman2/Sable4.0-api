bf1012dc804dccbe4b39eb23642cdc6feb30f1f0
razaman2 <razamvmt@gmail.com>
2021-01-20 11:17:49 -0500

    add ssl to server

--- cloud-run/deploy/post_deploy.sh
+++ cloud-run/deploy/post_deploy.sh
@@ -4,6 +4,7 @@ cd /app || exit
 
 sed -i "s,SABLE_PORT,${PORT},g" /etc/nginx/nginx.conf
 sed -i "s,SABLE_SERVER_NAME,${APP_NAME},g" /etc/nginx/nginx.conf
+sed -i "s,SABLE_APP_URL,${APP_URL},g" /etc/nginx/nginx.conf
 
 # start the application
 php-fpm -D && nginx -g "daemon off;"
