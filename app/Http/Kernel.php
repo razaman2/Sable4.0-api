7927ff7af6901fac1973835a3b191f453775d02a
razaman2 <razamvmt@gmail.com>
2021-01-21 10:56:37 -0500

    updated response headers

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,7 +34,7 @@
                 \Illuminate\View\Middleware\ShareErrorsFromSession::class,
                 \App\Http\Middleware\VerifyCsrfToken::class,
                 \Illuminate\Routing\Middleware\SubstituteBindings::class,
-                \App\Http\Middleware\AddResponseHeaders::class,
+                //\App\Http\Middleware\AddResponseHeaders::class,
             ],
 
             'api' => [
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,52 +1,46 @@
 <?php
 
-namespace App\Providers;
+    namespace App\Providers;
 
-use Helpers\ADC\ADCAuth;
-use Helpers\Docusign\Auth\DocusignAuthFactory;
-use Helpers\Docusign\Docusign;
-use Illuminate\Support\ServiceProvider;
-use Laravel\Telescope\TelescopeServiceProvider;
+    use Helpers\ADC\ADCAuth;
+    use Helpers\Docusign\Auth\DocusignAuthFactory;
+    use Helpers\Docusign\Docusign;
+    use Illuminate\Support\Facades\URL;
+    use Illuminate\Support\ServiceProvider;
+    use Laravel\Telescope\TelescopeServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    class AppServiceProvider extends ServiceProvider
     {
-        $this->app->bind(ADCAuth::class, function() {
-            return new ADCAuth(
-                request()->input('credentials.username', env('ADC_USERNAME')),
-                request()->input('credentials.password', env('ADC_PASSWORD')),
-                request()->input('credentials.branchID', 0),
-                request()->input('credentials.twoFactor', '')
-            );
-        });
+        /**
+         * Register any application services.
+         * @return void
+         */
+        public function register() {
+            $this->app->bind(ADCAuth::class, function() {
+                return new ADCAuth(request()->input('credentials.username', env('ADC_USERNAME')), request()->input('credentials.password', env('ADC_PASSWORD')), request()->input('credentials.branchID', 0), request()->input('credentials.twoFactor', ''));
+            });
 
-        $this->app->bind(Docusign::class, function() {
-            return new Docusign(request()->input('test') ? DocusignAuthFactory::authenticate([
-                'Username' => env('DOCUSIGN_DEV_USERNAME'),
-                'Password' => env('DOCUSIGN_DEV_PASSWORD'),
-                'IntegratorKey' => env('DOCUSIGN_DEV_INTEGRATOR_KEY'),
-            ])->setHost(env('DOCUSIGN_DEV_HOST')) :
+            $this->app->bind(Docusign::class, function() {
+                return new Docusign(request()->input('test') ? DocusignAuthFactory::authenticate([
+                    'Username' => env('DOCUSIGN_DEV_USERNAME'),
+                    'Password' => env('DOCUSIGN_DEV_PASSWORD'),
+                    'IntegratorKey' => env('DOCUSIGN_DEV_INTEGRATOR_KEY'),
+                ])->setHost(env('DOCUSIGN_DEV_HOST')) :
 
-                DocusignAuthFactory::authenticate(request()->input('auth'))->setHost(env('DOCUSIGN_PRO_HOST')));
-        });
+                    DocusignAuthFactory::authenticate(request()->input('auth'))->setHost(env('DOCUSIGN_PRO_HOST')));
+            });
 
-        $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-        $this->app->register(TelescopeServiceProvider::class);
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
+
+        /**
+         * Bootstrap any application services.
+         * @return void
+         */
+        public function boot() {
+            if(env('APP_ENV') !== 'local') {
+                URL::forceScheme('https');
+            }
+        }
     }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-}
