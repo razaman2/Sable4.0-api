c56b2af41d324ae77b90b2fa9944ec877c75de82
razaman2 <razamvmt@gmail.com>
2021-01-30 02:33:44 -0500

    added features and updated to use the reflection class

--- app/helpers/Builder/MethodInvoker.php
+++ app/helpers/Builder/MethodInvoker.php
@@ -4,22 +4,28 @@
     class MethodInvoker
     {
         private $object;
+        private $responses;
+        private $private;
 
-        public function __construct($object) {
+        public function __construct(object $object, bool $private = false) {
             $this->object = $object;
+            $this->private = $private;
         }
 
-        public function invoke($data, $payload = '') {
+        public function invoke($data, ...$payload) {
+            $object = new \ReflectionClass($this->object);
+
             foreach($data as $key => $value) {
                 $name = $this->normalize($key);
 
-                if(method_exists($this->object, $name)) {
-                    call_user_func([
-                        $this->object,
-                        $name,
-                    ], $value, $payload);
+                if($object->hasMethod($name)) {
+                    $method = $object->getMethod($name);
+                    $method->setAccessible($this->private);
+                    $this->responses[$key] = $method->invoke($this->object, $value, ...$payload);
                 }
             }
+
+            return (object) $this->responses;
         }
 
         protected function normalize($method) {
